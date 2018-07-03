5d17bf74fb6f21ea5a696c27ba25e2be687e1fe1
Tianyi <Tianyi>
2018-07-03 20:02:27 +0800

    doing 6-9

--- api/main.go
+++ api/main.go
@@ -26,11 +26,22 @@ func RegisterHandlers() *httprouter.Router {
 	log.Printf("preparing to post request\n")
 	router:=httprouter.New()
 	router.POST("/user", CreateUser)
-	router.POST("/user/:user_name", Login)
+	router.POST("/user/:username", Login)
+	router.GET("/user/:username", GetUserInfo)
+	router.POST("/user/:username/videos", AddNewVideo)
+	router.GET("/user/:username/videos", ListAllVideos)
+	router.DELETE("/user/:username/videos/:vid-id", DeleteVideo)
+	router.POST("/videos/:vid-id/comments", PostComment)
+	router.GET("/videos/:vid-id/comments", ShowComments)
 	return router
 }
 
+func Prepare() {
+	session.LoadSessionsFromDB()
+}
+
 func main() {
+	Prepare()
 	r:=RegisterHandlers()
 	mh:=NewMiddleWareHandler(r)
 	http.ListenAndServe(":8000", mh)
